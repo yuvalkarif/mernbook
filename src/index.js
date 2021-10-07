3619c4a1685a6aaeb6e1267f62836df26ba04996
yuvalkarif <yuvalkarif3@gmail.com>
2021-10-07 13:01:32 +0300

    mongoose and models

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import express from "express";
+import mongodb from "./configs/database";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const db = mongodb();
+
 app.get("/", (req, res) => {
   res.send("welcome to node with babel");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Express applictaion is listening on port ${port}`);
 });
---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  displayname: { type: String, required: true },
+  password: { type: String, required: true },
+  following: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+  followers: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+  posts: [{ type: mongoose.Types.ObjectId, ref: "Post" }],
+  picture: { type: String },
+  about: {
+    summary: { type: String },
+    work: { type: String },
+    education: { type: String },
+    birthday: { type: Date },
+  },
+});
+
+const User = model("User", userSchema);
+export default User;
