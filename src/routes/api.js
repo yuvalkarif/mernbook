39a76d05ab5549ddbf487484fb15392fcfc35983
yuvalkarif <yuvalkarif3@gmail.com>
2021-10-10 12:44:37 +0300

    checklist update

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,7 +6,7 @@ let router = Router();
 //User
 router.post("/signup", userController.signup); // (username,password,displayname)
 router.post("/login", userController.login); // (username,password)
-router.get("/currentUser", userController.currentUser); // to be logged in ()
+router.get("/current-user", userController.currentUser); // to be logged in ()
 router.get("/user", userController.getUser); // (id)
 router.patch("/user", userController.updateUser); // (id, picture?, summary?, work?, education?, birthday?)
 router.patch("/follow", userController.followUser); // (id,userId)
@@ -25,21 +25,23 @@ router.delete("/comment", postController.removeComment);
 export default router;
 
 /*
-    x API Route-Manage all CRUD Options
-    V User
-    v -Login
-    v -Register
-    V -Get User Profile
-    V -Update
-     V -Follow
-     V -Unfollow
-     V -Edit About
-    V Posts
-    V -Get All Posts By Followed
-    V -Add new Post
-    V -Edit your Posts
-    V -Like/Unlike
-    V -Comment 
-     x -Edit
-     x -Like/Unlike
+
+    [x] API Route-Manage all CRUD Options
+    [v]User
+    [v]---Login
+    [v]---Register
+    [v]---Get User Profile
+    [v]---Update
+    [v]---Follow
+    [v]---Unfollow
+    [v]---Edit About
+    [v]Posts
+    [v]---Get All Posts By Followed
+    [v]---Add new Post
+    [v]---Edit your Posts
+    [v]---Like/Unlike
+    [v]Comment 
+    [x]--Edit
+    [x]--Like/Unlike
+
 */
