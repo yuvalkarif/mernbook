ed89d8870d26ba781f908272c068fd155cb1f26a
yuvalkarif <yuvalkarif3@gmail.com>
2021-10-15 13:05:28 +0300

    post writer almost finished

--- client/src/components/post-writer/PostWriter.styles.ts
+++ client/src/components/post-writer/PostWriter.styles.ts
@@ -1,44 +1,104 @@
 import styled from "styled-components";
 import {
   bigIcon,
+  smallIcon,
   Container,
   MediumImage,
   TextBox,
   WideButton,
+  Button,
 } from "../styled/styledTheme";
-import { Photo } from "@styled-icons/material-rounded";
+import { Photo, Upload } from "@styled-icons/material-rounded";
 
 export const WriterContainer = styled(Container)`
+  margin-bottom: 1rem;
+  & > div:last-of-type {
+    margin-top: 0.5em;
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+    }
+  }
+  &:focus-within {
+    textarea {
+      min-height: 6em;
+    }
+  }
+`;
+
+export const TopContainer = styled.div`
   & > div {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     padding-bottom: 1rem;
     border-bottom: 1px solid ${({ theme }) => theme.btnBg};
-    textarea {
+    > textarea {
       ${TextBox}
       font-weight: 500;
       font-size: 1.1rem;
       transition: min-height 250ms ease-out;
       min-height: 0;
-      &:focus {
+      &:focus-within {
         min-height: 6em;
       }
     }
   }
-  margin-bottom: 1rem;
+  & > img {
+    margin: 0 auto;
+    align-self: center;
+    overflow: hidden;
+    width: 100%;
+  }
 `;
 
 export const WriterProfile = styled(MediumImage)``;
 
 export const ActionButton = styled.button`
   ${WideButton}
-  margin-block:0.5em;
-  max-width: fit-content;
+  width:unset;
+  min-width: fit-content;
   padding: 0.5rem 1rem;
+  span {
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
 `;
 
 export const PhotoIcon = styled(Photo)`
   ${bigIcon}
-  color: ${({ theme }) => theme.altAccent}
+  color: ${({ theme }) => theme.altAccent};
+  @media (max-width: 768px) {
+    margin-right: 0 !important;
+  }
 `;
+export const PostButton = styled.button`
+  ${WideButton}
+  max-width: fit-content;
+  padding: 0.75rem 1rem;
+  background-color: ${({ theme }) => theme.accentBtnBg};
+  color: ${({ theme }) => theme.accentBtnText};
+  height: 100%;
+`;
+export const LinkInput = styled.input`
+  ${TextBox}
+  width: 100%;
+`;
+
+export const UploadIcon = styled(Upload)`
+  ${bigIcon}
+  margin: .5rem;
+  width: 100%;
+  :hover {
+    color: ${({ theme }) => theme.altAccent};
+    cursor: pointer;
+  }
+`;
+
+export const LinkContainer = styled.div``;
--- client/src/components/post-writer/PostWriter.tsx
+++ client/src/components/post-writer/PostWriter.tsx
@@ -1,26 +1,89 @@
-import { useContext } from "react";
+import { useContext, useState, useRef } from "react";
 import {
   WriterContainer,
   WriterProfile,
   ActionButton,
   PhotoIcon,
+  PostButton,
+  LinkInput,
+  UploadIcon,
+  TopContainer,
+  LinkContainer,
 } from "./PostWriter.styles";
 import UserContext from "../../constants/context";
 import TextareaAutosize from "react-textarea-autosize";
-
+import { FormatListBulletedDimensions } from "@styled-icons/material-rounded/FormatListBulleted";
+interface PostWriterType {
+  body: string;
+  picture?: string;
+}
+interface Image {
+  src: string;
+  isValid: boolean;
+  isChecking: boolean;
+}
 export const PostWriter = () => {
   const { user } = useContext(UserContext);
+  const [showLink, setShowLink] = useState<Boolean>(false);
+  const [post, setPost] = useState<PostWriterType>({
+    body: "",
+    picture: "",
+  });
+
+  const [image, setImage] = useState<Image>({
+    src: "",
+    isValid: false,
+    isChecking: false,
+  });
+  const imageCheck = () => {
+    setImage({ ...image, isChecking: true });
+  };
+
   return (
     <WriterContainer>
-      <div>
-        <WriterProfile src={user?.picture} />
-        <TextareaAutosize placeholder="Whats on your mind?" />
-      </div>
+      <TopContainer>
+        <div>
+          <WriterProfile src={user?.picture} />
+          <TextareaAutosize
+            placeholder="Whats on your mind?"
+            onChange={(e) => setPost({ ...post, body: e.target.value })}
+            value={post.body}
+          />
+        </div>
+        {image.isChecking && (
+          <img
+            src={image.src}
+            alt=""
+            onError={() =>
+              setImage({ isChecking: false, src: "", isValid: false })
+            }
+            onLoad={() =>
+              setImage({ ...image, isChecking: true, isValid: true })
+            }
+          />
+        )}
+      </TopContainer>
 
-      <ActionButton>
-        <PhotoIcon />
-        Add a photo
-      </ActionButton>
+      <div>
+        <div>
+          <ActionButton onClick={() => setShowLink(!showLink)}>
+            <PhotoIcon />
+            <span>Add a photo</span>
+          </ActionButton>
+          {showLink && (
+            <LinkContainer>
+              <LinkInput
+                placeholder=".png, .jpg or .webp links"
+                onChange={(e) => setImage({ ...image, src: e.target.value })}
+                value={image.src}
+              />
+              <UploadIcon onClick={imageCheck} />
+            </LinkContainer>
+          )}
+        </div>
+
+        <PostButton onClick={() => console.log(post)}>Post</PostButton>
+      </div>
     </WriterContainer>
   );
 };
