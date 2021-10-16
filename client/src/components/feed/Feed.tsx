e3bbc32accb065e085d299029d0048804b5a807e
yuvalkarif <yuvalkarif3@gmail.com>
2021-10-16 14:20:54 +0300

    fixing loading more

--- client/src/components/feed/Feed.tsx
+++ client/src/components/feed/Feed.tsx
@@ -5,29 +5,35 @@ import { FeedWrapper } from "./Feed.styles";
 export const Feed = ({ posts }: { posts: string[] | undefined }) => {
   const [postsIds, setPostsIds] = useState<string[] | undefined>();
   const [postsToRender, setPostsToRender] = useState<string[] | undefined>();
+  const [numOfPosts, setNumOfPosts] = useState<number>(5);
 
   useEffect(() => {
     if (!postsIds) {
       setPostsIds(posts);
     } else if (postsIds && !postsToRender) {
-      setPostsToRender([...postsIds].slice(-5));
-      setPostsIds();
+      console.log(postsIds);
+      setPostsToRender([...postsIds].slice(-numOfPosts));
     } else if (postsIds && postsToRender) {
       setPostsToRender([...postsToRender, ...postsIds.slice(-1)]);
+      setNumOfPosts(numOfPosts + 1);
     }
   }, [posts, postsIds]);
 
   const handleMore = () => {
-    setPostsToRender;
+    if (postsIds && postsToRender) {
+      setNumOfPosts(numOfPosts + 5);
+
+      setPostsToRender([...postsIds.slice(-numOfPosts)]);
+    }
   };
   return (
     <div>
       <PostWriter setPostsIds={setPostsIds} />
       <FeedWrapper>
         {postsToRender &&
-          postsToRender.map((post, i) => {
+          postsToRender.slice().map((post, i) => {
             console.log(`post ${i} ${post}`);
-            return <Post key={i} postId={post} />;
+            return <Post key={post} postId={post} />;
           })}
       </FeedWrapper>
       <button type="button" onClick={handleMore}>
