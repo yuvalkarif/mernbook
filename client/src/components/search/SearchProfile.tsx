f9b5b79ae22c25acde0ed46debf8f934bb23658a
Yuval Karif <34835592+yuvalkarif@users.noreply.github.com>
2021-10-22 10:59:30 +0300

    conflict fix

--- client/src/components/search/SearchProfile.tsx
+++ client/src/components/search/SearchProfile.tsx
@@ -1,10 +1,5 @@
 import { User } from "../../constants/interfaces";
-<<<<<<< Updated upstream
-import { BigAvatar, ProfileWrapper } from "./Search.styles";
-export const SearchProfile = ({ user }: { user: User }) => {
-  return (
-    <ProfileWrapper>
-=======
+
 import { BigAvatar, ProfileWrapper, ShortcutIcon } from "./Search.styles";
 import { useHistory } from "react-router";
 import { Shortcut } from "@styled-icons/material-rounded";
@@ -15,16 +10,12 @@ export const SearchProfile = ({ user }: { user: User }) => {
   };
   return (
     <ProfileWrapper onClick={handleClick}>
->>>>>>> Stashed changes
       <BigAvatar src={user.picture} />
       <div>
         <span>{user.displayname}</span>
         <span>{user?.about?.work || user?.about?.education}</span>
       </div>
-<<<<<<< Updated upstream
-=======
       <ShortcutIcon />
->>>>>>> Stashed changes
     </ProfileWrapper>
   );
 };
