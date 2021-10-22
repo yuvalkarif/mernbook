68dbb40c7ad2cb6c9b3f6ad3c189f66a5d50a433
Yuval Karif <34835592+yuvalkarif@users.noreply.github.com>
2021-10-22 15:28:50 +0300

    routing constants

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React, { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { lazily } from "react-lazily";
-import { User } from "./constants/interfaces";
 import "./App.css";
-import { checkUser } from "./helpers/api";
 import { useAuth } from "./hooks/useAuth";
 import UserContext from "./constants/context";
 import ProtectedRoute from "./components/routes/ProtectedRoute";
+import ROUTES from "./constants/routes";
 
 // import Login from "./components/login/Login";
 const { Login } = lazily(() => import("./components/login/Login"));
@@ -24,19 +23,19 @@ function App() {
         <Router>
           <Suspense fallback={<p>Loading...</p>}>
             <Switch>
-              <ProtectedRoute path={"/"} exact>
+              <ProtectedRoute path={ROUTES.DASHBOARD} exact>
                 <Dashboard />
               </ProtectedRoute>
-              <ProtectedRoute path={"/p/:username"} exact>
+              <ProtectedRoute path={ROUTES.PROFILE + ":username"} exact>
                 <Profile />
               </ProtectedRoute>
-              <ProtectedRoute path={"/search"} exact>
+              <ProtectedRoute path={ROUTES.SEARCH} exact>
                 <Search />
               </ProtectedRoute>
-              <ProtectedRoute path="/login" exact alt>
+              <ProtectedRoute path={ROUTES.LOGIN} exact alt>
                 <Login checkForUser={checkForUser} />
               </ProtectedRoute>
-              <Route path="/signup" exact>
+              <Route path={ROUTES.SIGNUP} exact>
                 <Signup />
               </Route>
             </Switch>
